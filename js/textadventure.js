5717b300f8544798f3eaf41830c863c1d34d7239
David Alliet <davida.dc@gmail.com>
2015-12-28 18:38:24 +0100

    Basic interface and command parsing

--- js/textadventure.js
+++ js/textadventure.js
@@ -1,58 +1,115 @@
 // The main textadventure object that will hold most of the functionality to control the game
 var TextAdventure = (function (){
 
-   var container = null;
-   var inputField = null;
+  var container = null;
+  var inputField = null;
+  var taHeight = 0;
 
-   // init the game, gets passed the <div> container
-   function init(cid) {
-      console.log("Initializing text adventure");
-      container = document.getElementById(cid);
-      prepareContainer();
-   }
+  // init the game, gets passed the <div> container and the desired height of the box
+  function init(cid, h) {
+    console.log("Initializing text adventure");
+    container = document.getElementById(cid);
+    taHeight = h;
+    prepareContainer();
+  }
 
-   function prepareContainer() {
-      console.log("Preparing container with game elements");
-      container.innerHTML = "";
+  function prepareContainer() {
+    console.log("Preparing container with game elements");
+    container.innerHTML = "";
 
-      // create a container for the inputfield
-      var inputContainer = document.createElement("div");
-      inputContainer.className = "dal-ta-input";
+    // container for the output of the game
+    var outputContainer = document.createElement("div");
+    outputContainer.className = "dal-ta-output";
 
-      // create the inputfield
-      inputField = document.createElement("input");
-      inputField.type = "text";
-      inputField.name = "dal-ta-inputfield";
-      inputField.addEventListener("keypress", function(e){
-         TextAdventure.parseCommand(this.value);
-      }, true);
-      // append inputfield to field container:
-      inputContainer.appendChild(inputField);
-      // append the container to the text adventure container in the dom:
-      container.appendChild(inputContainer);
-   }
+    // append container for output to dom
+    container.appendChild(outputContainer);
 
-   // public function to expose the container object
-   function getContainer() {
-      return container;
-   }
+    // container for the input field
+    var inputContainer = document.createElement("div");
+    inputContainer.className = "dal-ta-input";
 
-   // public function to expose the input field
-   function getInputField() {
-      return inputField;
-   }
+    // create field and set properties
+    inputField = document.createElement("input");
+    inputField.type = "text";
+    inputField.name = "dal-ta-inputfield";
+    inputField.placeholder = "Enter command followed by RETURN";
+    inputField.autofocus = true;
+    inputField.focus();
 
-   function parseCommand(c) {
-      console.log("parsing command "+ c);
-      console.log(inputField.value);
-   }
+    // add the event handler
+    inputField.addEventListener("keypress", function(e){
 
-   // expose the functions in the return object
-   return {
-      init: init,
-      getContainer: getContainer,
-      getInputField: getInputField,
-      parseCommand: parseCommand
-   };
+      if(e.keyCode===13) {
+        // User hit enter, sending the command to the parser
+        TextAdventure.parseCommand(this.value);
+
+        // empty placeholder as user has entered a valid string
+        if(this.placeholder!=="") {
+          this.placeholder = "";
+        }
+        // empty the input field
+        inputField.value="";
+      }
+    }, true);
+
+    // append input to container
+    inputContainer.appendChild(inputField);
+    // append container for input to dom
+    container.appendChild(inputContainer);
+
+    // set height of the text adventure container and its elements
+    container.style.height = taHeight+"px";
+    outputContainer.style.height = (taHeight - inputContainer.clientHeight)+"px";
+  }
+
+  // public function to expose the container object
+  function getContainer() {
+    return container;
+  }
+
+  // public function to expose the input field
+  function getInputField() {
+    return inputField;
+  }
+
+  function parseCommand(c) {
+    console.log("Received command : "+ c);
+
+    // convert the entire command into lower case
+    c = c.toLowerCase();
+    // break command into spaces
+    var cl = c.split(" ");
+
+    // evaluate command based on first element:
+    switch(cl[0]) {
+        case "help":
+          console.log("Displaying help");
+          break;
+
+        case "inventory":
+          console.log("Displaying the inventory");
+          break;
+
+        case "move":
+          if(cl[1]===undefined) {
+            console.log("No move direction specified");
+          } else {
+            console.log("Moving in direction "+ cl[1]);
+          }
+          break;
+
+        default:
+          console.log("unknown command");
+          break;
+    }
+  }
+
+  // expose the public functions in the return object
+  return {
+    init: init,
+    getContainer: getContainer,
+    getInputField: getInputField,
+    parseCommand: parseCommand
+  };
 
 })();
