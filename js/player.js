131ef0761a13ba91d0e5e6424521c574beee7cd9
David Alliet <davida.dc@gmail.com>
2016-01-01 14:45:17 +0100

    Basic player object and inventory + location functionality

---
+++ js/player.js
@@ -0,0 +1,60 @@
+// The player object will hold functionalities and data related specifically to the player
+var Player = (function() {
+
+  var currentLocation = "";
+  var atObject = "";
+  var inventory = {};
+
+  function init(si) {
+    console.log("Initializing the player object");
+    inventory = si;
+  }
+
+  // allows to get the current location of the player
+  function getLocation() {
+    return currentLocation;
+  }
+
+  // allows to set the current location of the player
+  function setLocation(l) {
+    currentLocation = l;
+  }
+
+
+  // inventory functions
+  // *******************
+
+  // checks if an item is in the inventory,
+  // get's passed a string, returns false or the object with all its properties
+  function inInventory(item) {
+    for(var object in inventory) {
+      if(object===item) return true;
+    }
+    return false;
+  }
+
+  // adds an item to the inventory
+  function addItemToInventory(id, item) {
+    inventory[id] = item;
+  }
+
+  // returns a specific item from the inventory based on an id
+  function getItemFromInventory(id) {
+    return inventory[id];
+  }
+
+  // returns the full inventory
+  function getInventory() {
+    return inventory;
+  }
+
+  return {
+    init: init,
+    getLocation: getLocation,
+    setLocation: setLocation,
+    getInventory: getInventory,
+    addItemToInventory: addItemToInventory,
+    getItemFromInventory: getItemFromInventory,
+    inInventory: inInventory
+  };
+})();
