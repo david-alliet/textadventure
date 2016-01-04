a008c87bfc7e8b0cdf21fae288edd8376d01bad0
David Alliet <davida.dc@gmail.com>
2016-01-04 09:31:22 +0100

    Moved starting directory to seperate file

---
+++ gamedata/inventory.js
@@ -0,0 +1,12 @@
+var inventory = {
+  "test object": {
+    description: "An object for testing the startin inventory system.",
+    can_use: true,
+    text_on_use: "The object responds.",
+    can_pickup: true,
+    text_on_pickup: "",
+    can_use_on_object: false,
+    text_on_use_object_on: "",
+    remove_after_use: false
+  }
+}
